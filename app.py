567f3acdd1fc44d4084a2fc571817eba85a30c44
Naomi <naomijiang0211@gmail.com>
2024-12-09 18:22:14 -0500

    fixed minor issue

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from functools import wraps
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from flask_jwt_extended import JWTManager
 
 # Load environment variables
 load_dotenv()
